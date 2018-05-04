a7af0f3410c2856ebf51b6bf81dd40455053ae63
Manoj Panta <manojpanta95@gmail.com>
2018-05-04 10:47:28 -0600

    working on how to show specials for a comedian

--- spec/features/user_sees_all_comedians_spec.rb
+++ spec/features/user_sees_all_comedians_spec.rb
@@ -1,10 +1,13 @@
 RSpec.describe 'Visitors' do
   it 'should show all comedians' do
-    comedian = Comedian.create(name: travis, age: 30)
+    name = 'travis'
+    age = 30
+    comedian = Comedian.create(name: name, age: age)
 
     visit('/comedians')
     save_and_open_page
-
-    expect(page).to have_content(attributes)
+    expect(page).to have_content(name)
+    expect(page).to have_content(age)
+    expect(page).to have_content
   end
 end

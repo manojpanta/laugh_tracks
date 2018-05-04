a628861dd625229d73150b81f43bb8fbbc3fc1b8
Manoj Panta <manojpanta95@gmail.com>
2018-05-04 10:58:29 -0600

    times up

--- spec/features/user_sees_all_comedians_spec.rb
+++ spec/features/user_sees_all_comedians_spec.rb
@@ -5,9 +5,30 @@ RSpec.describe 'Visitors' do
     comedian = Comedian.create(name: name, age: age)
 
     visit('/comedians')
-    save_and_open_page
     expect(page).to have_content(name)
     expect(page).to have_content(age)
-    expect(page).to have_content
+  end
+
+  it 'should show average age for comedians' do
+    name = 'travis'
+    age = 30
+    comedian = Comedian.create(name: name, age: age)
+    comedian = Comedian.create(name: name, age: age)
+    comedian = Comedian.create(name: name, age: age)
+
+    visit('/comedians')
+    expect(page).to have_content('Average_age = 30.0')
+  end
+
+  it 'should show comedian with age of 34' do
+    name = 'travis'
+    age = 34
+    comedian = Comedian.create(name: name, age: age)
+    comedian = Comedian.create(name: name, age: age)
+    comedian = Comedian.create(name: name, age: age)
+
+    visit('/comedians')
+    save_and_open_page
+    expect(page).to have_content('Average_age = 30')
   end
 end
